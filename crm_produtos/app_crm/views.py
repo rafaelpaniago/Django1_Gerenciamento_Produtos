2742f40df7415c75638dba3198eb721eab5f3e6f
Rafael Paniago <115939524+rafaelpaniago@users.noreply.github.com>
2024-04-28 15:59:55 -0300

    Django para criação das páginas
    Três páginas criadas para teste: home / produtos / clientes.
    
    Utilizei o DJANGO para passar o caminho dos templates HTML das três página.

--- crm_produtos/app_crm/views.py
+++ crm_produtos/app_crm/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'app_crm/home.html')
+
+def produtos(request):
+    return render(request, 'app_crm/produtos.html')
+
+def clientes(request):
+    return render(request, 'app_crm/clientes.html')
